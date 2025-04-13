be7030a1d4e8aedad06000045175d31fce64c26e
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-04-14 01:58:24 +0530

    Posting tweets deleting them and viewing on tweet_list,etc these functionalities are added.

--- backtweet/tweet/models.py
+++ backtweet/tweet/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from django.contrib.auth.models import User
 
 class Tweet(models.Model):
@@ -10,4 +9,4 @@ class Tweet(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user.username}: {self.content[:20]}..."  # Display first 20 characters of the tweet
+        return f"{self.user.username}: {self.text[:20]}..."  # Display first 20 characters of the tweet
